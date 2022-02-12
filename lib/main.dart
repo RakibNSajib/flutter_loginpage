87bd2a67cf786b33a494d2a0bb4e12d73d04e5d3
Rakib Hossain Sajib <rakibnsajib@gmail.com>
2022-02-13 01:45:42 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'loginpage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: const LoginPage(),
+    );
+  }
+}
